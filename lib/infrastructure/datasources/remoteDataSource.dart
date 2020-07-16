e03115f2c3ba298fc9585c3d52cc6ec4627f20d8
ditu474 <daniel_ramirez82181@elpoli.edu.co>
2020-07-16 10:43:56 -0500

    update

--- lib/infrastructure/datasources/remoteDataSource.dart
+++ lib/infrastructure/datasources/remoteDataSource.dart
@@ -11,20 +11,26 @@ import 'package:http/http.dart' as http;
 
 class RemoteDataSource implements IRemoteDataSource {
   final http.Client client;
-  final String urlAPI =
-      'http://aulatallerrestfulapp-env.eba-v6d9ykru.us-east-1.elasticbeanstalk.com/';
+  final String urlAPI = 'https://rich-solstice-283505.ue.r.appspot.com';
 
   RemoteDataSource({@required this.client});
 
   @override
   Future<User> loginWithCredentials({Email email, Password password}) async {
     try {
-      final response = await client.post(urlAPI + 'api/v1/users/login',
-          headers: {
-            'Content-Type': 'application/json',
+      final response = await client.post(
+        urlAPI + 'api/v1/users/login',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            "correo": email.value,
+            "password": password.value,
           },
-          body:
-              json.encode({"correo": email.value, "password": password.value}));
+        ),
+      );
+      print(response.body);
       if (response.statusCode == 200) {
         return UserModel.fromMap(json.decode(response.body));
       } else
